e52e826294ea85da44f716b52a3c31caec852013
Martin Schinz <martin.schinz@pebblecode.com>
2014-03-26 10:33:29 +0000

    start

---
+++ Library1.fs
@@ -0,0 +1,26 @@
+﻿namespace XunitTest
+
+open Xunit
+
+type TheseDontRun () =
+    [<Fact>]
+    member this.ShouldNotRun () =
+        failwith "Oh snap, i ran!"
+
+[<Trait("Category", "Unit")>]
+type TheseWillRun () =
+    [<Fact>]
+    member this.ShouldRun () =
+        Assert.True(true)
+
+module ``These won't run`` =
+    [<Fact>]
+    let ``I will throw if i run`` () =
+        failwith "Oh noes!"
+
+[<Trait("Category", "Unit")>]
+module ``But these will``=
+    [<Fact>]
+    let ``I will run`` () =
+        Assert.True(true)
+
